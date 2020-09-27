35941dfa81246473d5afe1a1b8eb69849c386f47
calvchen <ch981129@126.com>
2020-09-27 11:16:05 +0800

    my_repulsion_loss.py  modified 9/27/2020

--- nets/RepulsionLoss/repulsion_loss.py
+++ nets/RepulsionLoss/repulsion_loss.py
@@ -14,14 +14,21 @@ def IoG(box_a, box_b):
     I = Iw * Ih                                                                                                    
     G = (box_b[2] - box_b[0]) * (box_b[3] - box_b[1])                                                              
     return I / G                                                                                                   
-	
-def RepGT(pred_boxes, gt_rois, rois_inside_ws):
+
+
+def smooth_ln(input, delta=0.9):
+    output = torch.where(torch.ls(input, delta), -torch.log(1-input), (input-delta)/(1-delta)-torch.log(1-input))
+    return output
+
+
+def RepGT(pred_boxes, gt_rois, rois_inside_ws):   #B, G
 
     sigma_repgt = 0.9
     loss_repgt=torch.zeros(pred_boxes.shape[0]).cuda()                                                                                                                                                      
     for i in range(pred_boxes.shape[0]):                                                                                                                                                                                       
         boxes = Variable(pred_boxes[i,rois_inside_ws[i]!=0].view(int(pred_boxes[i,rois_inside_ws[i]!=0].shape[0])/4,4))     
         gt = Variable(gt_rois[i,rois_inside_ws[i]!=0].view(int(gt_rois[i,rois_inside_ws[i]!=0].shape[0])/4,4))              
+
         num_repgt = 0
         repgt_smoothln=0
         if boxes.shape[0]>0:
@@ -34,7 +41,8 @@ def RepGT(pred_boxes, gt_rois, rois_inside_ws):
             for j in range(max_overlaps.shape[0]):
                 if max_overlaps[j]>0:
                     num_repgt+=1
-                    iog = IoG(boxes[j], gt[argmax_overlaps[j]])
+                    iog = IoG(boxes[j], gt[argmax_overlaps[j]])  #G, P
+
                     if iog>sigma_repgt:
                         repgt_smoothln+=((iog-sigma_repgt)/(1-sigma_repgt)-math.log(1-sigma_repgt))
                     elif iog<=sigma_repgt:
@@ -78,8 +86,8 @@ def RepBox(pred_boxes, gt_rois, rois_inside_ws):
             
     return loss_repbox
 				   
-def repulsion(rois, box_deltas, gt_rois, rois_inside_ws, rois_outside_ws):                                             
-
+def repulsion(rois, box_deltas, gt_rois, rois_inside_ws, rois_outside_ws):
+    #
 
     deltas = Variable(box_deltas.view(rois.shape[0],256,4))                                                        
     rois_inside_ws = Variable(rois_inside_ws.view(rois.shape[0],256,4))                                            
@@ -94,7 +102,7 @@ def repulsion(rois, box_deltas, gt_rois, rois_inside_ws, rois_outside_ws):
 
     pred_boxes = bbox_transform_inv(rois[:,:,1:5], deltas, 2)
 
-    loss_RepGT = RepGT(pred_boxes, gt_rois, rois_inside_ws)
+    loss_RepGT = RepGT(pred_boxes, gt_rois, rois_inside_ws)   #pred & true
     loss_RepBox = RepBox(pred_boxes, gt_rois, rois_inside_ws)
 
     return loss_RepGT, loss_RepBox
